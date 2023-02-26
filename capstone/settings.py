6e35a14540d8052d3a8252ff4e2680d3e24e7a56
Salah Sajar <sajarsalaheldine@gmail.com>
2023-02-26 16:28:22 +0100

    expose django secret key for cs50w staff

--- capstone/settings.py
+++ capstone/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO__SECRETKEY
+SECRET_KEY = 'django-insecure-czur#l@76!8prt!q3m6v&i5p#ua80nu!@4jfyzrx%(x*6gzswh'
+# SECRET_KEY = DJANGO__SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
