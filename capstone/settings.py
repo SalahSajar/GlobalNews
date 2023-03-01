4c493724f0c19b6e4eed2607ea8eaf7efae59c1f
Salah Sajar <sajarsalaheldine@gmail.com>
2023-03-01 23:15:53 +0100

    configare project to serve static files for vercel

--- capstone/settings.py
+++ capstone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = DJANGO__SECRETKEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
