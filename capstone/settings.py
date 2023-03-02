f3a364fa04529b01b18a0e3f1c24aeedb4f0d535
Salah Sajar <sajarsalaheldine@gmail.com>
2023-03-02 15:16:07 +0100

    manually build static files [I GAVE UP]

--- capstone/settings.py
+++ capstone/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.Middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
