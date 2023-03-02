44202a8143f5bb1544019d12f0c7762780728155
Salah Sajar <sajarsalaheldine@gmail.com>
2023-03-02 13:03:47 +0100

    day 2 of trying to configare my project to serve static files

--- capstone/settings.py
+++ capstone/settings.py
@@ -17,8 +17,7 @@ DJANGO__SECRETKEY = env("DJANGO__SECRETKEY")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = DJANGO__SECRETKEY
@@ -83,12 +82,12 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
 
 AUTH_USER_MODEL = "globalNews.User"
 
