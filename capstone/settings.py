a1a96549528b4290c58ce996e5608c68684cb2e2
Salah Sajar <sajarsalaheldine@gmail.com>
2023-03-02 14:58:08 +0100

    manually build static files [I GAVE UP]

--- capstone/settings.py
+++ capstone/settings.py
@@ -17,7 +17,7 @@ DJANGO__SECRETKEY = env("DJANGO__SECRETKEY")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'globalNews/static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'globalNews/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'globalNews/static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = DJANGO__SECRETKEY
@@ -139,8 +139,3 @@ STATICFILES = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "globalNews/static/globalNews/assets")
-
-if os.environ.GET("VERCEL"):
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
