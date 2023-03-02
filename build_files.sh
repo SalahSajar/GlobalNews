32bafddc6c17be3063b2e31c4659c25a40edaf10
Salah Sajar <sajarsalaheldine@gmail.com>
2023-03-02 12:43:38 +0100

    day 2 of trying to configare my project to serve static files

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 echo "BUILD START"
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 pip install -r requirements.txt
-python manage.py collectstatic
 echo "BUILD END"
