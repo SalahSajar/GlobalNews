c5088e640118726d25ab5c4408d73049e72b7431
Salah Sajar <sajarsalaheldine@gmail.com>
2023-03-03 17:08:28 +0100

    No module named 'psycopg2' Error [FIXING]

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo "BUILD START"
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-pip install psycopg2
-pip install psycopg2-binary
+pip install --upgrade setuptools
 echo "BUILD END"
